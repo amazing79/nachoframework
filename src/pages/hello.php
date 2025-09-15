74e92a0915e98c079bccbc200fd7866554ce3808
amazing79 <ignacioja@hotmail.com>
2025-09-14 23:46:24 -0300

    ajustes para separar logica de rutas (config) de nuestro framework

--- src/pages/hello.php
+++ src/pages/hello.php
@@ -1,4 +1,2 @@
-<?php
 
-$name = $request->query->get('name', 'World') ?>
-Hello <?= htmlspecialchars($name, ENT_QUOTES, 'UTF-8');
+Hello <?= htmlspecialchars($name ?? 'World', ENT_QUOTES, 'UTF-8');
