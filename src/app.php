96292b2c39a82f1ad19b60c253c9699341b42f04
amazing79 <ignacioja@hotmail.com>
2025-09-16 15:13:58 -0300

    The httpkernel component: The controller resolver
    Se agrega la libreria de symfony http-kernel, que resuelve problema de rendimiento si instanciamos clases controladores por cada ruta.
    Esta libreria tambien ayuda a inyectarle parametros a los metodos de nuestros controladores (similar usan Laravel, laminas y slim, entre otros).
    Ademas nos provee una capa para manejos de errorres cuando no encontramos el controlador correspondiente o nos equivocamos con los parametros.
    En esta version del microframework, eliminar la funcion para procesar el resto de los templates. No queda claro si por error u omision.
    Con o sin esto, nuestro pequeño framework alcanza para crear paginas o app tipo blogs sencillas.

--- src/app.php
+++ src/app.php
@@ -3,6 +3,18 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
 
+class LeapYearController
+{
+    public function index(?int $year = null): Response
+    {
+        if (is_leap_year($year)) {
+            return new Response('Yep, this is a leap year!');
+        }
+
+        return new Response('Nope, this is not a leap year.');
+    }
+
+}
 function is_leap_year(?int $year = null): bool
 {
     if (null === $year) {
@@ -15,30 +27,7 @@ function is_leap_year(?int $year = null): bool
 $routes = new Routing\RouteCollection();
 $routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', [
     'year' => null,
-    '_controller' => function (Request $request): Response {
-        if (is_leap_year($request->attributes->get('year'))) {
-            return new Response('Yep, this is a leap year!');
-        }
-
-        return new Response('Nope, this is not a leap year.');
-    }
-]));
-$routes->add('bye', new Routing\Route('/bye', [
-    '_controller' => function (Request $request): Response {
-        return render_template($request);
-    }
-]));
-$routes->add('hello', new Routing\Route('/hello/{name}', [
-    'name' => 'world',
-    '_controller' => function (Request $request): Response {
-        // $foo will be available in the template
-        $request->attributes->set('foo', 'testing a new value');
-        $response = render_template($request);
-        // change some header
-        $response->headers->set('Content-Type', 'text/plain');
-
-        return $response;
-    }
+    '_controller' => 'LeapYearController::index'
 ]));
 
 return $routes;
