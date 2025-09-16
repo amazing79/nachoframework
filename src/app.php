9a1767a50acab0b71d817d21d29135aef3bb528f
amazing79 <ignacioja@hotmail.com>
2025-09-16 15:38:54 -0300

    The separation of concerns
    Se refactorizo codigo para crear nuestra libreria reutilizable. Simplex/Framework.
    Se organizo codigo en funcion de controlador y modelo.
    Se refactorizo el punto de entrada de nuestra app, para trabjar con nuestro pequeño framework.
    Tambien se refactorizaron rutas y composer para trabajar con esta nueva estructura y namespace.
    No se instalo nada nuevo, solo organizo codigo.

--- src/app.php
+++ src/app.php
@@ -1,33 +1,10 @@
 <?php
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
-
-class LeapYearController
-{
-    public function index(?int $year = null): Response
-    {
-        if (is_leap_year($year)) {
-            return new Response('Yep, this is a leap year!');
-        }
-
-        return new Response('Nope, this is not a leap year.');
-    }
-
-}
-function is_leap_year(?int $year = null): bool
-{
-    if (null === $year) {
-        $year = (int)date('Y');
-    }
-
-    return 0 === $year % 400 || (0 === $year % 4 && 0 !== $year % 100);
-}
 
 $routes = new Routing\RouteCollection();
 $routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', [
     'year' => null,
-    '_controller' => 'LeapYearController::index'
+    '_controller' => 'Calendar\Controller\LeapYearController::index'
 ]));
 
 return $routes;
--- web/front.php
+++ web/front.php
@@ -3,7 +3,6 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel;
 use Symfony\Component\Routing;
 
@@ -17,17 +16,7 @@ $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 $controllerResolver = new HttpKernel\Controller\ControllerResolver();
 $argumentResolver = new HttpKernel\Controller\ArgumentResolver();
 
-try {
-    $request->attributes->add($matcher->match($request->getPathInfo()));
-
-    $controller = $controllerResolver->getController($request);
-    $arguments = $argumentResolver->getArguments($request, $controller);
-
-    $response = call_user_func_array($controller, $arguments);
-} catch (Routing\Exception\ResourceNotFoundException $exception) {
-    $response = new Response('Not Found', 404);
-} catch (Exception $exception) {
-    $response = new Response('An error occurred', 500);
-}
+$framework = new Simplex\Framework($matcher, $controllerResolver, $argumentResolver);
+$response = $framework->handle($request);
 
 $response->send();
