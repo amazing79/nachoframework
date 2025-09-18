1f8efa0348f3b61e560964ecd00dace161e073a3
amazing79 <ignacioja@hotmail.com>
2025-09-18 15:03:52 -0300

    The EventDispatcher component
    Se agrego libreria para manejo de eventos de Symfony.
    Se adapto el framework para manejarlos. Ademas se crearon clases de ejemplos para analizar su funcionamiento.
    Se opto por solo adaptar la version optima del framework, pero dejandolo funcional.

--- web/front.php
+++ web/front.php
@@ -5,6 +5,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel;
 use Symfony\Component\Routing;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
 $request = Request::createFromGlobals();
 $routes = include __DIR__.'/../src/app.php';
@@ -13,10 +14,13 @@ $context = new Routing\RequestContext();
 $context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
+$dispatcher = new EventDispatcher();
+$dispatcher->addSubscriber(new Simplex\EventsListener\ContentLengthListener());
+
 $controllerResolver = new HttpKernel\Controller\ControllerResolver();
 $argumentResolver = new HttpKernel\Controller\ArgumentResolver();
 
-$framework = new Simplex\Framework($matcher, $controllerResolver, $argumentResolver);
+$framework = new Simplex\Framework($dispatcher, $matcher, $controllerResolver, $argumentResolver);
 $response = $framework->handle($request);
 
 $response->send();
